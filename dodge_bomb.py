c2fee2e9056cdf14dd463faf41ecf9a550ce0135
masaya <c0a2207092@edu.teu.ac.jp>
2023-04-25 16:59:38 +0900

    演習１未完成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,6 +10,20 @@ delta = {
         pg.K_RIGHT: (+1, 0),
         }
 
+def init_kk_imgs():  # 追加機能１未完 
+    kk_img = pg.image0.load("ex02/fig/3.png")  # 追加機能１未完 
+    kk_img0 = pg.transform.rotozoom(kk_img, 0, 2.0)  # 追加機能１未完 
+    return{
+            (0, -1): pg.transform.rotozoom(kk_img, 270, 1.0),   # 追加機能１未完 
+            (+1, -1): pg.transform.rotozoom(kk_img, 315, 1.0), # 追加機能１未完 
+            (+1, 0): pg.transform.rotozoom(kk_img, 0, 1.0),  # 追加機能１未完 
+            (+1, +1): pg.transform.rotozoom(kk_img, 45, 1.0),  # 追加機能１未完 
+            (0, +1): pg.transform.rotozoom(kk_img, 90, 1.0),  # 追加機能１未完 
+            (-1, +1): pg.transform.rotozoom(kk_img, 135, 1.0),  # 追加機能１未完 
+            (-1, 0): pg.transform.rotozoom(kk_img, 180, 1.0),  # 追加機能１未完 
+            (-1, -1): pg.transform.rotozoom(kk_img, 225, 1.0),  # 追加機能１未完 
+            }
+
 def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
     オブジェクトが画面内or画面買いを判定し、真理値タプルを返す関数
@@ -30,8 +44,8 @@ def main():
     screen = pg.display.set_mode((1600, 900))
     clock = pg.time.Clock()
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img = kk_img0  # 追加機能１未完 
+    kk_img = init_kk_imgs[+1, 0]  # 追加機能１未完 
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     bb_img = pg.Surface((20, 20))  # 練習1
@@ -57,11 +71,16 @@ def main():
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
+        for l, nv in kk_kai.items():
+            if key_lst[l]:
+                bb_rct.move_ip(nv)
 
         if check_bound(screen.get_rect(),kk_rct) != (True, True):
             for k, mv in delta.items():
                 if key_lst[k]:
                     kk_rct.move_ip(-mv[0], -mv[1])  
+
+        if main[0] == 0
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
@@ -76,8 +95,11 @@ def main():
             return #練習６
         pg.display.update()
         clock.tick(1000)
-
-
+"""     for r in range(1,11):
+            bb_img = pg.Surface((20*r, 20*r))
+            pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+            bb_imgs.append(bb_img)
+"""
 if __name__ == "__main__":
     pg.init()
     main()
